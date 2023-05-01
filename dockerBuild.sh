3fbe8185f6fce41574b6ac7f9a88c1c4a599465e
OnkarD <2021mt93590@wilp.bits-pilani.ac.in>
2023-05-01 18:10:33 +0530

    bug fix docker tagging statement in build file

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -5,19 +5,19 @@
 echo "Working with employee-service"
 cd ./../employee-service
 docker build --tag=emp-service .
-docker tag emp-service odbits:emp-service
-docker push odbits:emp-service
+docker tag emp-service odbits/emp-service
+docker push odbits/emp-service
 
 echo "Working with address-service"
 cd ./../address-service
 docker build --tag=address-service .
-docker tag address-service odbits:address-service
-docker push odbits:address-service
+docker tag address-service odbits/address-service
+docker push odbits/address-service
 
 echo "Working with project-service"
 cd ./../project-service
 docker build --tag=project-service .
-docker tag project-service odbits:project-service
-docker push odbits:project-service
+docker tag project-service odbits/project-service
+docker push odbits/project-service
 
 echo "COMPLETED"
